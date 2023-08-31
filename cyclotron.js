bcd048f3aedca6c303ec9f98b68a819ddd3e53fb
Lucas Vieira <ls6182315@gmail.com>
2023-08-31 16:49:23 -0300

    some fix

--- cyclotron.js
+++ cyclotron.js
@@ -1,11 +1,11 @@
-function cyclotron(particle,size) {
+function cyclotron(size,particle) {
     const matrix = Array.from({ length: size }, () => Array.from({ length: size }, () => '1'));
   
     if (particle === 'e') {
       for (let i = 1; i < size; i++) {
         for (let j = 0; j < size; j++) {
-          matrix[0][j] = 'e';
-          matrix[j][size -1] = 'e'
+          matrix[0][j] = particle;
+          matrix[j][size -1] = particle
         }
       }
     } else if (particle === 'p') {
@@ -14,13 +14,13 @@ function cyclotron(particle,size) {
           if (i === 0 || j === 0 || i === size - 1 || j === size - 1) {
             matrix[i][j] = 'p';
             matrix[size -1][size -1] = '1'
-            matrix[size -2][size -2] = 'p'
+            matrix[size -2][size -2] = particle
           }
         }
       }
     } else if (particle === 'n') {
         for (let i = 0; i < size; i++) {
-      matrix[0][i] = 'n';
+      matrix[0][i] = particle;
         }
     } else {
       for (let i = 1; i < size; i++) {
@@ -33,10 +33,18 @@ function cyclotron(particle,size) {
     return matrix;
   }
   
+  // Cyclotron without particles:
   console.log(cyclotron(5)); 
-  console.log(cyclotron('e',4)); 
-  console.log(cyclotron('p',4)); 
-  console.log(cyclotron('p',6));
-  console.log(cyclotron('n',4)); 
+
+  // Accelerating an electron:
+  console.log(cyclotron(4, 'e')); 
+
+  // Accelerating a proton:
+  console.log(cyclotron(4,'p')); 
+  // Case: 6x6
+  console.log(cyclotron(6,'p'));
+
+  // Accelerating a neutron:
+  console.log(cyclotron(4,'n')); 
 
   
